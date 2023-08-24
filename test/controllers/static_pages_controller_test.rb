8f0c6fb839234161aac95fbe085c89790c9e8081
Koya <558koya@gmail.com>
2023-08-24 15:22:49 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
+    assert_select "title", "ホームページ | DAB"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "ヘルプ | DAB"
   end
 end
