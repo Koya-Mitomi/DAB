e56184b9ec4ca0d6f5f34708f744e0b7c5b8b2a5
Koya <558koya@gmail.com>
2023-08-31 04:10:18 +0900

    complete expenditure

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_034856) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_183938) do
+  create_table "expenditures", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expenditures_on_user_id"
+  end
+
   create_table "incomes", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
@@ -29,5 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_034856) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "expenditures", "users"
   add_foreign_key "incomes", "users"
 end
