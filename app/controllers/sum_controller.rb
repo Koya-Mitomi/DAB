ddca20bcb862a6a8f495a4cce7055b80932e0e10
Koya <558koya@gmail.com>
2023-09-05 17:35:19 +0900

    年ごと、月ごとの収支確認切り替え機能を追加

--- app/controllers/sum_controller.rb
+++ app/controllers/sum_controller.rb
@@ -24,5 +24,27 @@ class SumController < ApplicationController
   end
 
   def check
+    if params[:year].present?
+      @income_amounts = current_user.income_amounts.where(
+        date: Time.new(params[:year], 1, 1).all_year
+      )
+      @expenditure_amounts = current_user.expenditure_amounts.where(
+        date: Time.new(params[:year], 1, 1).all_year
+      )
+    elsif params["date(1i)"].present?
+      @income_amounts = current_user.income_amounts.where(
+        date: Time.new(params["date(1i)"], params["date(2i)"], params["date(3i)"]).all_month
+      )
+      @expenditure_amounts = current_user.expenditure_amounts.where(
+        date: Time.new(params["date(1i)"], params["date(2i)"], params["date(3i)"]).all_month
+      )
+    else
+      @income_amounts = current_user.income_amounts.where(
+        date: Time.zone.today.all_month
+      )
+      @expenditure_amounts = current_user.expenditure_amounts.where(
+        date: Time.zone.today.all_month
+      )
+    end
   end
 end
