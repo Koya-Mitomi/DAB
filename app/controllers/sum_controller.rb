703d1f259d7c9aa00e44e93a1287a02bd558e46c
Koya <558koya@gmail.com>
2023-09-06 12:38:06 +0900

    complete sum-compare

--- app/controllers/sum_controller.rb
+++ app/controllers/sum_controller.rb
@@ -28,23 +28,41 @@ class SumController < ApplicationController
       @income_amounts = current_user.income_amounts.where(
         date: Time.new(params[:year], 1, 1).all_year
       )
+      @pre_i = current_user.income_amounts.where(
+        date: Time.new(params[:year], 1, 1).years_ago(1).all_year
+      ).sum(:amount)
       @expenditure_amounts = current_user.expenditure_amounts.where(
         date: Time.new(params[:year], 1, 1).all_year
       )
+      @pre_e = current_user.expenditure_amounts.where(
+        date: Time.new(params[:year], 1, 1).years_ago(1).all_year
+      ).sum(:amount)
     elsif params["date(1i)"].present?
       @income_amounts = current_user.income_amounts.where(
         date: Time.new(params["date(1i)"], params["date(2i)"], params["date(3i)"]).all_month
       )
+      @pre_i = current_user.income_amounts.where(
+        date: Time.new(params["date(1i)"], params["date(2i)"], params["date(3i)"]).months_ago(1).all_month
+      ).sum(:amount)
       @expenditure_amounts = current_user.expenditure_amounts.where(
         date: Time.new(params["date(1i)"], params["date(2i)"], params["date(3i)"]).all_month
       )
+      @pre_e = current_user.expenditure_amounts.where(
+        date: Time.new(params["date(1i)"], params["date(2i)"], params["date(3i)"]).months_ago(1).all_month
+      ).sum(:amount)
     else
       @income_amounts = current_user.income_amounts.where(
         date: Time.zone.today.all_month
       )
+      @pre_i = current_user.income_amounts.where(
+        date: Time.zone.today.months_ago(1).all_month
+      ).sum(:amount)
       @expenditure_amounts = current_user.expenditure_amounts.where(
         date: Time.zone.today.all_month
       )
+      @pre_e = current_user.expenditure_amounts.where(
+        date: Time.zone.today.months_ago(1).all_month
+      ).sum(:amount)
     end
   end
 end
