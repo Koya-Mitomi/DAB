8c7aaf31e226ce06014de75092ac235e0eb5a485
Koya <558koya@gmail.com>
2023-09-10 00:18:13 +0900

    change validation

--- app/models/expenditure_amount.rb
+++ app/models/expenditure_amount.rb
@@ -3,6 +3,6 @@ class ExpenditureAmount < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :expenditure_id, presence: true
-  validates :amount, presence: true, numericality: { in: 0..1000000000000 }
+  validates :amount, presence: true, numericality: { in: 0..2000000000 }
   validates :date,   presence: true
 end
--- app/models/income_amount.rb
+++ app/models/income_amount.rb
@@ -3,6 +3,6 @@ class IncomeAmount < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :income_id, presence: true
-  validates :amount, presence: true, numericality: { in: 0..1000000000000 }
+  validates :amount, presence: true, numericality: { in: 0..2000000000 }
   validates :date,   presence: true
 end
